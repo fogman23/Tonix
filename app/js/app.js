3679cae86591bb6dac4bf11928bea17e2a50d52d
Valera <fogman23@gmail.com>
2021-07-22 19:04:18 +0500

    Верстка комнопента диапазона цен и подключение плагина для его работы

--- app/js/app.js
+++ app/js/app.js
@@ -1,13 +1,23 @@
 // // Import jQuery module (npm i jquery)
-// import $ from 'jquery'
-// window.jQuery = $
-// window.$ = $
+import $ from 'jquery'
+window.jQuery = $
+window.$ = $
+import { ionRangeSlider } from 'ion-rangeslider'
 
 // // Import vendor jQuery plugin example (not module)
 // require('~/app/libs/mmenu/dist/mmenu.js')
 
 document.addEventListener('DOMContentLoaded', () => {
 
-	// Custom JS
+	$("#rangePrice").ionRangeSlider({
+		type: "double",
+		min: 0,
+		max: 15000,
+		from: 5000,
+		to: 10000,
+		grid: false,
+		postfix: '₽',
+		decorate_both: true
+	})
 
 })
